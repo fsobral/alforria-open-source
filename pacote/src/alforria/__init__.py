efa80d6fd3b45af79d93ac5cb9a60eda78526b09
GabSeven <gabrodrigomes@gmail.com>
2025-12-29 16:46:44 -0300

    feat: add serialize to classes
    now classes Professor and Turmas can be JSON's\n verify if the tuples to object conversion are optimal

--- pacote/src/alforria/__init__.py
+++ pacote/src/alforria/__init__.py
@@ -1,5 +1,11 @@
 """Pacote Alforria"""
 
-__version__ = "0.1"
+__version__ = "0.2"
 
-from .alforria import *
+from . import main
+
+
+def set_config_path(path: str):
+    main._PATHS_PATH = path + "/paths.cnf"
+    main._ALFCFG_PATH = path + "/alforria.cnf"
+    main._CONST_PATH = path + "/con stantes.cnf"
---
+++ pacote/src/teste.py
@@ -0,0 +1,7 @@
+import alforria
+
+# print(alforria._PATHS_PATH)
+
+print(alforria.main._PATHS_PATH)
+alforria.set_config_path("./teste")
+print(alforria.main._PATHS_PATH)
