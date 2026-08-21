0bc1ace534abac93f4fbbb6083a8b3a40a8d69a1
GabSeven - arch <gabrodrigomes@gmail.com>
2026-08-20 22:05:58 -0300

    delete teste.py
    arquivo estava dentro do pacote sem fazer nada

--- pacote/src/teste.py
+++
@@ -1,9 +0,0 @@
-import alforria.__main__ as __main__
-
-# print(alforria._PATHS_PATH)
-
-print(__main__._PATHS_PATH)
-__main__.set_config_path("./teste")
-print(__main__._PATHS_PATH)
-
-print(__main__.__version__)
